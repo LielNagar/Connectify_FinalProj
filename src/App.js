098f66b4500e12e62242d09f54d91a451f85b0ba
LielNagar <104072448+LielNagar@users.noreply.github.com>
2023-01-05 03:51:00 +0200

    login page, signup page works with db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
+import HomePage from './components/HomePage';
+import Post from './components/Post';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<LoginPage />}></Route>
+          <Route exact path='/SignUp' element={<SignUpPage />}></Route>
+          <Route exact path='/HomePage/:userId' element={<HomePage />}></Route>
+          <Route exact path='/Search' element={<Search />}></Route>
+          <Route exact path='/MyProfile' element={<p>MyProfile</p>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import LoginPage from './components/LoginPage';
+import PostContextProvider from './components/PostContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <PostContextProvider>
+        <App />
+    </PostContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
