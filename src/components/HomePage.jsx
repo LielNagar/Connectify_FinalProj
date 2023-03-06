58589872a11ef4df23805f87508642ef46e28229
LielNagar <104072448+LielNagar@users.noreply.github.com>
2023-03-07 01:32:12 +0200

    feature/init

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,11 @@
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 import React, { useState, useContext } from "react";
 import { useLocation } from "react-router-dom";
 
 //COMPONENTS
-import AllPosts from "./AllPosts";
 import Menu from "./Menu";
-import { PostContext } from "./PostContext";
+import LiveChat from "./LiveChat";
+import Dashboard from "./Dashboard";
+import DataPosts from "./DataPosts";
 
 export default function HomePage() {
   let { state: user } = useLocation();
@@ -17,27 +16,9 @@ export default function HomePage() {
   return (
     <div className="HomePage">
       <Menu user={user} />
-      <TextField
-        style={{
-          width: "700px",
-          marginTop: "120px",
-          marginLeft: "27%",
-          marginRight: "30%",
-        }}
-        multiline={true}
-        rows={4}
-        placeholder="Tell us something...."
-        onChange={(e) => setPost(e.target.value)}
-      />
-      <br />
-      <Button
-        variant="text"
-        style={{ marginLeft: "47%", marginRight: "30%" }}
-        onClick={() => addPost(post, user)}
-      >
-        Add Post!
-      </Button>
-      <AllPosts user={user} state="feed" currentId={user.Id}/>
+      <LiveChat />
+      <DataPosts user={user}/>
+      <Dashboard />
     </div>
   );
 }
